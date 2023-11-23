6890786b0d72db1d180377721d626e0d7653ed43
junhaa <wooha34567@gmail.com>
2023-11-23 17:37:14 +0900

    Exception 인터페이스 추가

---
+++ src/main/java/umc/study/apiPayload/code/BaseCode.java
@@ -0,0 +1,10 @@
+package umc.study.apiPayload.code;
+
+import java.security.cert.CertPathValidatorException;
+
+public interface BaseCode {
+
+    public ReasonDTO getReason();
+
+    public ReasonDTO getReasonHttpStatus();
+}
